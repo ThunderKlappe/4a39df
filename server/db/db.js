71ffddcb17e97376b89264e6a1bc4495a72f45e3
Tim Schley <timschley@gmail.com>
2022-04-24 23:49:29 -0700

    Set up Database

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://@localhost:5432/messenger",
+  {
+    logging: false,
+    dialect: "postgres",
+    username: "timschley",
+    password: "pass",
+  }
+);
 
 module.exports = db;
