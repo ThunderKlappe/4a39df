75431b9724f5942d8affc03a44549d3f7c415af5
Tim Schley <timschley@gmail.com>
2022-04-27 14:54:14 -0700

    Started cypress spec file to test file upload

---
+++ client/cypress/integration/send-message-ticket.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+const alice = {
+  username: "Alice",
+  email: "alice@example.com",
+  password: "Z6#6%xfLTarZ9U",
+};
+const bob = {
+  username: "Bob",
+  email: "bob@example.com",
+  password: "L%e$xZHC4QKP@F",
+};
+
+describe("Add feature: send pictures through chat", () => {
+  it("setup", () => {
+    cy.signup(alice.username, alice.email, alice.password);
+    cy.logout();
+    cy.signup(bob.username, bob.email, bob.password);
+    cy.logout();
+  });
+
+  it("selects a file", () => {
+    cy.login(alice.username, alice.password);
+
+    cy.get("input[name=search]").type("Bob");
+    cy.contains("Bob").click();
+
+    cy.get("input[type=file]")
+      .invoke("show")
+      .selectFile("src/assets/bg-img.png");
+    cy.contains("200");
+  });
+});
